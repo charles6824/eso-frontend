51d1e37305a689f2b65b856c7dc1b6f6c76ca46d
Greatnexx <godwindanielgodwin@gmail.com>
2024-08-25 18:55:57 +0100

    created new files

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+
+const Card = () => {
+  return (
+    <div className='h-[400px] w-[292px] rounded-[5px]'>
+        <img src="" alt="" />
+      
+    </div>
+  )
+}
+
+export default Card
---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' h-[710px] p-[100px] bg-[#ADDF881A]'>
+
+        <p className='w-[1240px] font-[400] leading-[30px] '>
+        The  Enterprise Support Organisation (ESO) program sharing and rating platform is a community resource developed by the Enterprise Support Organisation Collaborative which brings together the Ghana Enterprises Agency (GEA), National Entrepreneurship & Innovation Programme (NEIP), Ghana Hubs Network, Impact Investing Ghana, Mirepa Capital, InnoHub Foundation, Social Enterprise Ghana, MBC Africa, Reach for Change, ScaleUp Africa, and Center for Entrepreneurship & Evaluation Development. 
+      The platform is managed by Impact Investing Ghana and was built with funding from the Research and Innovation Systems in Africa (RISA) program of UK AID. 
+This platform provides a national database of all enterprise support programs in Ghana for entrepreneurs, startups and businesses to easily access so they can make informed decisions on what programs to participate in.  The platform also enables registered ESOs to improve the quality of their programmes and track their progress using the SCALE ESO improvement program developed by the Argidius Foundation. 
+
+The  Enterprise Support Organisation (ESO) program sharing and rating platform is a community resource developed by the Enterprise Support Organisation Collaborative which brings together the Ghana Enterprises Agency (GEA), National Entrepreneurship & Innovation Programme (NEIP), Ghana Hubs Network, Impact Investing Ghana, Mirepa Capital, InnoHub Foundation, Social Enterprise Ghana, MBC Africa, Reach for Change, ScaleUp Africa, and Center for Entrepreneurship & Evaluation Development. 
+The platform is managed by Impact Investing Ghana and was built with funding from the Research and Innovation Systems in Africa (RISA) program of UK AID. 
+This platform provides a national database of all enterprise support programs in Ghana for entrepreneurs, startups and businesses to easily access so they can make informed decisions on what programs to participate in.  The platform also enables registered ESOs to improve the quality of their programmes and track their progress using the SCALE ESO improvement program developed by the Argidius Foundation. 
+        </p>
+
+        
+        
+    </div>
+  )
+}
+
+export default About
